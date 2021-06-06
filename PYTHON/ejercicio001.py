ade8d85b726832f02923389bf5e9c21cc379dd10
Ninoska Cerezo <Ninoskac08@hotmail.com>
2021-06-06 18:37:43 -0500

    mi primer commit

---
+++ PYTHON/ejercicio001.py
@@ -0,0 +1,34 @@
+
+
+# # asi se declara una variable
+
+# nombre= 'Ninoska Cerezo Diaz'
+# edad= '21'
+# genero= 'F'
+# telefono= '0981148041'
+
+
+# # asi se declara una variable
+# print('El nombre del estudiante es: ' + nombre + ' y su edad es ' + str(edad) + ' y su genero es ' + genero + ' y su telefono es ' +telefono)
+
+
+# # esto es un comentario
+# # asi es otra forma de imprimir en pantalla
+# print(f'El nombre del estudiante es: {nombre} y su edad {edad} y su genero es {genero} y su telefono es {telefono}')
+
+
+
+
+
+
+
+
+
+primer_numero = int(input('por favor ingrese el primer numero : \t'))
+
+segundo_numero = float(input('por favor ingrese el segundo numero : \t'))
+
+resultado = primer_numero + segundo_numero
+
+print(resultado)
+print(type(resultado))
---
+++ PYTHON/ejercicio002.py
@@ -0,0 +1,14 @@
+# CALCULAR EL AREA Y EL PERIMETRO DEL CUADRADO 
+
+# DATOS
+lado= float(input('ingrese medida del lado del cuadrado:'))
+
+# OPERACION
+perimetro= lado * 4
+area= lado * lado
+
+# RESULTADO
+print('El perimetro del cuadrado es: ')
+print(perimetro)
+print('El area del cuadrado es: ')
+print(area)
