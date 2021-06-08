0aadc97fd9f313508413cec87b9faf964d9a81df
Ninoska Cerezo <Ninoskac08@hotmail.com>
2021-06-07 22:19:10 -0500

    Sring y mas

---
+++ PYTHON/ejercicio008.py
@@ -0,0 +1,35 @@
+"""
+
+STRING y mas!!!!
+"""
+esto_es_una_string = "Hola Ninoska como estas"
+esto_es_otra_string = "Mundo"
+
+# concatenar
+print(esto_es_una_string + ' ' + esto_es_otra_string)
+# hola mundo
+
+# MAYUS
+print(esto_es_una_string.upper())
+# minus
+print(esto_es_una_string.lower())
+# Primera en mayus
+print(esto_es_una_string.capitalize)
+# Pone en mayus la primera letra de cada palabra
+print(esto_es_una_string.title())
+# me dice la longitud del string
+print(len(esto_es_una_string))
+
+# buscar una caracter y muestra su ubicacion
+print(esto_es_una_string.find('e'))
+
+
+# rebanar!!!!!  o slice
+print(esto_es_una_string[0:2])  #tu le dices que inicie en cero
+print(esto_es_una_string[:2])  #el asume que inicie en cero
+print(esto_es_una_string[5:9])  
+
+# radar
+variable = 'Ninoska'
+print(variable[::1])  
+print(variable[::-1])
