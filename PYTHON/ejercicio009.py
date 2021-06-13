fea9bc82818a8af941c45b8ca16e94c144ed332a
Ninoska Cerezo <Ninoskac08@hotmail.com>
2021-06-12 21:45:15 -0500

    calculo del rectangulo

---
+++ PYTHON/ejercicio009.py
@@ -0,0 +1,15 @@
+# CALCULAR EL AREA Y EL PERIMETRO DEL RECTANGULO
+
+# DATOS
+base= float(input('ingrese medida de la base del rectangulo:'))
+altura= float(input('ingrese medida de la altura del rectangulo:'))
+
+# OPERACION
+area = base * altura
+perimetro = (base * 2) + (altura * 2)
+
+# RESULTADO
+print('El area del rectangulo es: ')
+print(area)
+print('El perimetro del rectangulo es: ')
+print(perimetro)
